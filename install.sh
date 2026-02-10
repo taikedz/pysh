02de3c9b1cd7fa55e55d69710e14518113e7c5e6
Tai Kedzierski <dch.tai@gmail.com>
2026-02-10 14:45:57 +0000

    Fix installer, add error bailout

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 HEREDIR="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p "$HOME/.local/bin"
-ln -s "$HOME/.local/bin/pysh" "$HEREDIR/pysh"
+ln -s "$HEREDIR/pysh" "$HOME/.local/bin/pysh"
 
 echo "Installed to $HOME/.local/bin/pysh . Ensure $HOME/.local/bin is on your \$PATH . If not, add it in your .bashrc"
 
