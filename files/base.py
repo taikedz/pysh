8e347e77d985e1b09df91278bef8bd3260cee62f
Tai Kedzierski <dch.tai@gmail.com>
2026-02-10 14:44:26 +0000

    Move wrapper function, add docstrings

--- files/base.py
+++ files/base.py
@@ -3,32 +3,11 @@ import pysh
 HERE = pysh.PySh(__file__)
 
 def main():
-    # your scripting here. Example:
-    HERE.args.positionals("+")
-    HERE.args.flags("--verbose", "--unsafe")
-    HERE.args.options(
-            ("host", "tower"),
-            ("user", "sm"),
-            ("port", 22),
-        )
-    args = HERE.args.parse()
-
-    # TODO - allow '--' separation
-    status, stdout, stderr = HERE.process(["ssh", "-p", str(args.port), f"{args.user}@{args.host}", HERE.shjoin(args.positionals)], text=True)
-
-    assert status == 0, f"Could not run remote command:\n{stderr}"
-    print(stdout)
+    # your scripting here.
 
 # =====
-
-def main_wrap():
-    try:
-        main()
-    except Exception as e:
-        if HERE.env("PY_TRACEBACK", "false").lower() == "true":
-            raise
-        else:
-            print(e)
-            return 1
-
-main_wrap()
+# Do not remove - this calls your main() function for you
+#
+# Stack traces are suppressed for user convenience.
+# Set PY_TRACEBACK=true in your environment to print them.
+pysh.main_wrap(main)
--- files/pysh.py
+++ files/pysh.py
@@ -8,6 +8,17 @@ import pathlib
 """ Convenience suite for system scripting in python
 """
 
+def main_wrap(_func):
+    try:
+        _func()
+    except (KeyboardInterrupt,Exception) as e:
+        if HERE.env("PY_TRACEBACK", "false").lower() == "true":
+            raise
+        else:
+            print(e)
+            exit(1)
+
+
 class PySh:
     def __init__(self, topfile):
         self._topfile = pathlib.Path(topfile)
@@ -17,18 +28,36 @@ class PySh:
 
 
     def open(self, filepath, mode='r'):
+        """ Open a file in the same directory as the current script
+        """
         return open(self._topdir/filepath, mode)
+
+
+    def path(self, path=''):
+        """ Resolve a path starting in the same directory as current script
+        """
+        return self._topfile / path
 
     
     def env(self, name, defval=None):
+        """ Get an environment variable
+        """
         return os.getenv(name, defval)
 
 
     def shjoin(self, tokens):
+        """ Join shell tokens
+        """
         return shlex.join(tokens)
 
 
     def process(self, command, text=False) -> tuple[int, str, str]:
+        """ Execute a single command subprocess, return the status, stdout and stderr.
+
+        Piping is not allowed.
+
+        Set `text=True` to return stdout and stderr as strings instead of byte strings
+        """
         if isinstance(command, str):
             command = shlex.split(command)
 
@@ -43,30 +72,70 @@ class PySh:
 
 
     def shell(self, command) -> None:
+        """ Run a shell command. Piping is allowed. Output is not captured.
+        """
         os.system(command, shell=True)
 
 
 class UserPysh:
-    def ask(self, prompt):
+    def ask(self, prompt) -> str:
+        """ Ask a user for input
+        """
         print(prompt, end='', flush=True)
         res = sys.stdin.readline()
         return res.strip("\n")
 
 
-    def confirm(self, prompt):
+    def confirm(self, prompt) -> bool:
+        """ Ask a user for a yes/no response as 'y' or 'yes'
+        Return True if y/yes , False otherwise
+        """
         res = self.ask(prompt)
         return res.lower() in ["yes", "y"]
 
 
     def choose(self, prompt, options):
-        raise NotImplementedError("not implemented. use ask()")
+        """ Display the list of options, ask user to choose from list via number
+
+        Return the chosen string
+
+        Raises ValueError if user entered non-int or value out of range
+        """
+        i = 1
+        for item in options:
+            print(f"  {i}: {item}")
+            i+=1
+
+        res = self.ask(prompt)
+        res = int(res)
+        if 0 < res and res <= len(res):
+            return options[res-1]
+        else:
+            raise ValueError("Out of range")
 
 
-    def name(self):
+    def name(self) -> str:
+        """ Return the current user name
+        """
         return os.getenv("USERNAME")
 
-    def uid(self):
-        return os.getenv("UID")
+    def uid(self) -> int:
+        """ Return the current user ID
+        """
+        return int(os.getenv("UID"))
+
+
+    def os(self) -> tuple[str,str]:
+        """ Return the OS name and version as of /etc/hosts (Linux and Unix only)
+        """
+        if not os.name == "posix":
+            # TODO - implement for Windows
+            raise RuntimeError("Only available on POSIX systems")
+
+        with open("/etc/hosts") as fh:
+            lines = [line.split("=", maxsplit=1) for line in fh.readlines()]
+        data = {k:v.strip('\n"') for k,v in lines}
+        return data["ID"], data["VERSION_ID"]
 
 
 class ArgumentParserPysh:
@@ -76,14 +145,20 @@ class ArgumentParserPysh:
 
 
     def parse(self):
+        """ Parse arguments as currently defined
+        """
         return self._parser.parse_args()
 
 
     def add_argument(self, *a, **k):
+        """ Add a raw argument to the parser. See argparse.ArgumentParser.add_argument(...)
+        """
         self._parser.add_argument(*a, **k)
 
 
     def positionals(self, *names):
+        """ Add named positional arguments, or '+' or '*'
+        """
         assert not self._positionals_locked, "INTENRAL FATAL: Positional args can no longer be added."
         if len(names) == 1 and names[0] in ["+", "*"]:
             self._parser.add_argument("positionals", nargs=names[0])
@@ -94,6 +169,8 @@ class ArgumentParserPysh:
 
 
     def flags(self, *flags):
+        """ Add boolean flags. During runtime, unset flags are flase, set flags are true
+        """
         for flag in flags:
             if not flag.startswith("--"):
                 if flag.startswith("-"):
@@ -103,8 +180,10 @@ class ArgumentParserPysh:
             self._parser.add_argument(flag, action="store_true")
 
 
-    def options(self, *opts):
-        for flag, defval in opts:
+    def options(self, **opts):
+        """ Add options with default values
+        """
+        for flag, defval in opts.items():
             if not flag.startswith("--"):
                 if flag.startswith("-"):
                     flag = f"-{flag}"
